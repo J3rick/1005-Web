feffd5aa8c0a1b8983d241ce7e40262fe504413d
stasuke0 <anguswee@hotmail.com>
2025-04-01 01:52:02 +0800

    added google authenticator code agains zzz

--- login.php
+++ login.php
@@ -30,6 +30,11 @@ require_once __DIR__ . '/inc/csrf.php';
         <input type="password" id="password" name="pwd" required>
       </div>
 
+      <div class="form-group">
+        <label for="2fa_code">Google Authenticator Code:</label>
+        <input type="text" id="2fa_code" name="2fa_code" required>
+      </div>
+
       <!-- CSRF token -->
       <?php csrfInputField(); ?>
       
@@ -46,11 +51,6 @@ require_once __DIR__ . '/inc/csrf.php';
         <a href="forgot-password.php">Forgot Password?</a>
       </div>
 
-      <div class="form-group">
-    <label for="2fa_code">Google Authenticator Code:</label>
-    <input type="text" id="2fa_code" name="2fa_code" required>
-      </div>
-      
     </form>
 </main>
     
