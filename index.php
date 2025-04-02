e25301600af7e40bf641e019f2b9414aaa28ffe0
Aydil <aydilkhair_9@hotmail.com>
2025-04-02 22:43:20 +0800

    minor changes

--- index.php
+++ index.php
@@ -179,7 +179,7 @@ $memorials = getMemorialCards($conn);
     <section class="memorials">
         <div class="section-header">
             <h3>Latest memorials</h3>
-            <a href="search.php?name=&dob_from=&dob_to=&dop_from=&dop_to=&religion=">Explore more »</a>
+            <a href="search.php?name=&dob_from=&dob_to=&dop_from=&dop_to=&religion=" style="color: #1a5b92;">Explore more »</a>
         </div>
         <div class="carousel-container">
             <div class="memorial-cards">
